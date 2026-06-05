965aece6b334629e91d9246b71d21f91f6c1017b
Frank <frankmuema42@gmail.com>
2026-06-05 12:08:19 +0300

    Implementing Logging and Middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from "helmet";
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(morgan('combined', {
+    stream: {
+        write: (message) => logger.info(message.trim()),
+    },
+}));
+
 app.get('/', (req, res) => {
+  logger.info('Hello from the Ikonex Academy API!');
   res.send('Hello and welcome to Ikonex Academy!');
+});
+
+// Health check
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', uptime: process.uptime(), timestamp: new Date().toISOString() });
+});
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Ikonex Academy API is working' });
 });
 
 export default app;
