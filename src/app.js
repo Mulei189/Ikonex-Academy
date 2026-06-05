022b2e93b811db8f40755523312070e7aee25519
Frank <frankmuema42@gmail.com>
2026-06-05 10:45:37 +0300

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello and welcome to Ikonex Academy!');
+  res.send('Hello and welcome to Ikonex Academy!');
 });
 
 export default app;
